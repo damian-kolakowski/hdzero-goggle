de15c17fcc7691426cecccc61eff607be43e65db
dimmoo <kolakowski.damian@gmail.com>
2022-12-23 07:26:36 +0100

    Fix setup.sh for aarch64

--- setup.sh
+++ setup.sh
@@ -11,10 +11,20 @@ if [ ! -d media ]; then
 	cat media.tar.gzaa media.tar.gzab | tar xz 
 fi
 
-if [ ! -d toolchain ]; then
-	echo "Extracting toolchain..."
-	tar xzvf toolchain.tar.gz
+
+arch=$(uname -p)
+if [ "$arch" = "x86_64" ]; then
+	if [ ! -d toolchain ]; then
+		echo "Extracting toolchain..."
+		tar xzvf toolchain.tar.gz
+	fi
+elif [ "$arch" = "aarch64" ];  then	
+	if [ ! -d toolchain_aarch64 ]; then
+		echo "Extracting toolchain..."
+		cat toolchain_aarch64.tar.gzaa toolchain_aarch64.tar.gzab toolchain_aarch64.tar.gzac toolchain_aarch64.tar.gzad | tar xz
+	fi	
 fi
+
 
 rm -rf build && mkdir build
 cmake . -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -Bbuild
